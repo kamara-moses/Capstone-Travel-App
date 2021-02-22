a2055238ff9368a502b9819f1d2413a539433dfb
Moses Kamara <kamara.moses@yahoo.com>
2021-02-21 21:14:36 -0800

    added webpack production environment

---
+++ webpack.prod.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/client/index.js",
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: "/\.js$/",
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ "style-loader", "css-loader", "sass-loader" ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i, 
+                loader: 'file-loader',
+                options: {
+                  name: './src/media/[name].[ext]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+            minify: false
+        })
+    ]
+}
