58ac7e21990f3913cd73d5d403aac0692bfe028b
Moses Kamara <kamara.moses@yahoo.com>
2021-02-21 21:52:43 -0800

    created the submit function

--- src/client/index.js
+++ src/client/index.js
@@ -1,2 +1,6 @@
+import { clickResponse }from "./js/handleSubmit"
+
 import "./styles/base.scss"
 import "./styles/footer.scss"
+
+export { clickResponse }
---
+++ src/client/js/handleSubmit.js
@@ -0,0 +1,9 @@
+const goButton = document.getElementById("generate");
+goButton.addEventListener("click", clickResponse);
+
+function clickResponse() {
+
+    console.log("::: Form Submitted :::")
+}
+
+export { clickResponse }
