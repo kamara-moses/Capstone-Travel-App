a474bf948cac460c00e0b7b5b52e4239f9a1a5be
Moses Kamara <kamara.moses@yahoo.com>
2021-02-25 12:39:49 -0800

    create geoName function

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -4,12 +4,34 @@ function clickRespond() {
 
     // Read values of city
     const city = document.getElementById("city").value;
-    console.log(city)
 
-    if (city === "") {
-        alert("Please enter a city name")
-    }
     console.log("::: Form Submitted :::")
+
+    postData("http://localhost:8000/geoName", {city: city})
+
+    .then(function(res) {
+        document.getElementById("results").innerHTML = `GeoData: ${res.geoData}`
+    })
 }
 
+
+const postData = async(city = "", data = {}) => {
+    console.log("GeoData:", data);
+    const response = await fetch(city, {
+        method: "POST",
+        credentials: "same-origin",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    });
+    try {
+        const nameData = await response.json();
+        console.log("Data received:", nameData)
+        return nameData;
+    } catch (error) {
+        console.log("error", error)
+    }
+}
 export { clickRespond } 
--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,6 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 // Setup empty JS object to act as endpoint for all routes
 let projectData = {};
 
@@ -11,6 +14,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,14 +24,36 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("dist"));
 
 
 // Setup Server
-const port = 8081;
+const port = 8000;
 const server = app.listen(port, listening);
 
 function listening() {
     console.log(`Server is running on port ${port}`);
 }
 
+// GET route
+app.get("/", function (req, res) {
+    res.sendFile("dist/index.html")
+})
+
+
+// API
+const geoNameURL = "http://api.geonames.org/searchJSON?q"
+const userName = process.env.USERNAME
+console.log(`Your Username is ${process.env.USERNAME}`)
+
+// POSt Route
+app.post("/geoName", async function(req, res) {
+    projectData = req.body.city;
+    console.log(`You entered: ${projectData}`);
+    const geoURL = `${geoNameURL}=${city}&maxRows=1&username=${userName}`
+
+    const response = await fetch(geoURL)
+    const geoData = await response.json()
+    console.log(goeData)
+    res.send(geoData)
+})
