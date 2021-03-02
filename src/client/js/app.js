8ad6db48d1479bb3ae4452bb5c352115efa3c78e
Moses Kamara <kamara.moses@yahoo.com>
2021-03-01 23:19:08 -0800

    refactor the functions back to original formation

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,44 +1,25 @@
-const API_KEY = "kamara.moses"
-
-const WEATHER_KEY = "94ec406ab85043f285fb0c27f57574a9"
-
-const PIXA_KEY = "19853981-85155ca595da994be43f034e6"
+const API_KEY = "&appid=7439220f89767ecc92468da6aaab2380"
 
 /* Global Variables */
 
 // The URL root if user searches by city
-const API_ROOT_CITY = "http://api.geonames.org/searchJSON?q=";
-
-const API_ROOT_PARAMS = "&maxRows=1&username="
-
-const API_ROOT_WEATHER = "https://api.weatherbit.io/v2.0/forecast/daily?"
-
-const API_WEATHER_PARAMS = "&lat=38.123&lon=-78.543&key="
-
-const API_ROOT_PIXA = "https://pixabay.com/api/?key="
-
-const API_PIXA_CITY = "&q="
-
-const API_PIXA_PARAMS = "&image_type=photo"
-
-
+const API_ROOT_CITY = 'http://api.openweathermap.org/data/2.5/weather?q=';
 
 // The URL for units parameter
-const API_UNITS = "&units=";
+const API_UNITS = '&units=';
 
 // Find the Generate button and add the listener
-const goButton = document.getElementById("generate");
-goButton.addEventListener("click", clickRespond);
+const goButton = document.getElementById('generate');
+goButton.addEventListener('click', clickRespond);
 
 // Main function of the program
-// Grabs the user"s input, then forms URL, calls API, POSTS and updates UI
+// Grabs the user's input, then forms URL, calls API, POSTS and updates UI
 function clickRespond() {
 
-   // Grab user"s input
-    const cityInput = document.getElementById("city");
-    const startDate = document.getElementById("depart");
-    const endDate = document.getElementById("return")
-    let units;
+   // Grab user's input
+    const cityInput = document.getElementById('city');
+    
+    let units; 
     let degreeSystem;
     if (units == "metric") {
         degreeSystem = "C";
@@ -49,17 +30,11 @@ function clickRespond() {
     // Read values of zip and city
     const city = cityInput.value;
 
-
     // Form URL based on zip or city search
     // (zip takes precendence if both were entered)
     let url;
     if (city) {
-    //     url = API_ROOT_CITY + city + API_ROOT_PARAMS + API_KEY
-    // } else {
-        url = API_ROOT_WEATHER + city + API_WEATHER_PARAMS + WEATHER_KEY
-    // }  {
-    //     url = API_ROOT_PIXA + PIXA_KEY + API_PIXA_CITY + API_PIXA_PARAMS;
-    // }
+        url = API_ROOT_CITY + city + API_UNITS + units + API_KEY;
     }
 
     // Call the API
@@ -68,20 +43,20 @@ function clickRespond() {
         // Prepares data for POST, calls the POST
         .then(function (weatherData) {
             console.log(weatherData)
-            const errorMessage = document.getElementById("error");
+            const errorMessage = document.getElementById('error');
             if (weatherData.cod == "200") {
-                errorMessage.classList.add("hide");
+                errorMessage.classList.add('hide');
                 const icon = weatherData.weather[0].icon;
-                const date = dateTime(startDate, endDate);
+                const date = dateTime();
                 const temperature = weatherData.main.temp.toFixed(0);
-                postJournal("/add", { icon, date, temperature });
+                postJournal('/add', { icon, date, temperature });
 
                 // Calls to update the site with latest entry
                 updateUI(degreeSystem);
 
             } else {
-                console.log("Bad data entered");
-                errorMessage.classList.remove("hide");
+                console.log('Bad data entered');
+                errorMessage.classList.remove('hide');
                 return;
             }
         })
@@ -98,31 +73,29 @@ async function getWeather(url) {
 // POSTs the journal data (icon, date/time, temperature, feelings)
 async function postJournal(url, data) {
     await fetch(url, {
-        method: "POST",
-        credentials: "same-origin",
-        headers: { 
-            "Content-Type": "application/json" 
-        },
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/json' },
         // Body data type must match "Content-Type" header        
         body: JSON.stringify(data)
     });
 }
 
-// Updates the website"s latest entry card
+
+// Updates the website's latest entry card
 // Includes weather icon, date, temperature, feelings
-// Shows the card if it"s hidden
+// Shows the card if it's hidden
 async function updateUI(degreeSystem) {
-    const response = await fetch("/retrieve");
+    const response = await fetch('/retrieve');
     const latestEntry = await response.json();
-    document.getElementById("icon").innerHTML = `<img class="icon" src="http://openweathermap.org/img/wn/${latestEntry.icon}@2x.png" alt="Weather icon">`
-    document.getElementById("date").innerHTML = `Date: ${latestEntry.date}`;
-    document.getElementById("temp").innerHTML = `Temperature: ${latestEntry.temperature}\xB0${degreeSystem}`;
-    document.getElementById("content").innerHTML = `Feelings: ${latestEntry.feelings}`;
-    document.getElementById("journal").classList.remove("hide");
+    document.getElementById('icon').innerHTML = `<img class="icon" src="http://openweathermap.org/img/wn/${latestEntry.icon}@2x.png" alt="Weather icon">`
+    document.getElementById('date').innerHTML = `Date: ${latestEntry.date}`;
+    document.getElementById('temp').innerHTML = `Temperature: ${latestEntry.temperature}\xB0${degreeSystem}`;
+    document.getElementById('content').innerHTML = `Feelings: ${latestEntry.feelings}`;
+    document.getElementById('journal').classList.remove('hide');
 }
 
-
-// Calculate the user"s date and time
+// Calculate the user's date and time
 // returns date and time in string
 function dateTime() {
     const d = new Date();
@@ -132,20 +105,6 @@ function dateTime() {
     }
     const date = `${d.getMonth() + 1}.${d.getDate()}.${d.getFullYear()} at time ${d.getHours()}:${minutes}`;
     return date;
-    // const today = new Date();
-    // const depart = new Date(startDate);
-    // const returnDate = new Date(endDate);
-    // let isSoon = false;
-
-    // const countdown = Math.round((depart.getTime() - today.getTime()) / (1000 * 60 * 60 * 24)); 
-    // const duration = Math.ceil((returnDate.getTime() - depart.getTime()) / (1000 * 60 * 60 * 24));
-
-    // if(countdown < 16) {
-    //     isSoon = true;
-    // }
-    // return { depart: startDate, duration: duration, countdown: countdown + 1, isSoon: isSoon};
 }
-
-
 
 export { clickRespond, getWeather, postJournal, updateUI, dateTime }
