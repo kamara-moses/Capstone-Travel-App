ef8fa3f8b91bf0d39ddc9a6aacc2752262cfbd03
Moses Kamara <kamara.moses@yahoo.com>
2021-03-09 22:52:56 -0800

    made changes to the server code

--- src/server/server.js
+++ src/server/server.js
@@ -43,9 +43,6 @@ app.post('/add', postData);
 
 function postData(request, response) {
     projectData = request.body;
-    // projectData['icon'] = request.body;
-    // // projectData['highTemp'] = projectData.response;
-    // // projectData['temp'] = projectData.lowTemp;
     response.send({ message: "Post received" })
     console.log(projectData)
 }
